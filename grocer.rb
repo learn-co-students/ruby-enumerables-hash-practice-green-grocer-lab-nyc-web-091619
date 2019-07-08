3c3b390e99a1620317554324666aa0a31d5255c8
Danny Lee <dreamer@panix.com>
2019-07-08 14:57:29 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,19 @@
+require 'pry'
+
 def consolidate_cart(cart)
   # code here
+  r = Hash.new(0)
+
+  cart.each do |item| 
+    r[item] = item
+    binding.pry
+    if r[r.keys[0]].has_key?(:count) != true    
+      r[r.keys[0]].store(:count,0)
+      r[r.keys[0]][:count] += 1
+    else
+      r[r.keys[0]][:count] += 1
+    end
+  end
 end
 
 def apply_coupons(cart, coupons)

d93efc6e7e6a2ff1366fa07a5f8fb0b9b267ae12
Danny Lee <dreamer@panix.com>
2019-07-10 02:00:58 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,29 +1,67 @@
 require 'pry'
 
 def consolidate_cart(cart)
-  # code here
-  r = Hash.new(0)
-
-  cart.each do |item| 
-    r[item] = item
-    binding.pry
-    if r[r.keys[0]].has_key?(:count) != true    
-      r[r.keys[0]].store(:count,0)
-      r[r.keys[0]][:count] += 1
+  r = {}      # don't use Hash.new(0)
+  
+  cart.each do |item|
+    
+    if not r.key?(item.keys.first)
+      r.update(item) { |key, value| value }
+      r[item.keys.first].store(:count,1)
     else
-      r[r.keys[0]][:count] += 1
+      r[item.keys.first][:count] += 1 
+    end
+  end
+  r
+end
+
+ 
+def apply_coupons(cart, coupon)
+  coupon.each do |cp|
+    if cart.keys.include?(cp[:item])
+      if cart[cp[:item]][:count] >= cp[:num]
+        addItem = "#{cp[:item]} W/COUPON"   
+                              # used "cart.keys[0] + "
+                              # vs "#{cp[:item]} W/COUPON" 
+        if !cart[addItem]
+          cart[addItem] = { 
+            price: cp[:cost] / cp[:num],
+            clearance: cart[cp[:item]][:clearance],
+            count: cp[:num]
+          }
+        else
+          cart[addItem][:count] += cp[:num]
+        end
+        cart[cp[:item]][:count] -= cp[:num]
+      end
+    end 
+  end
+  cart
+
+end
+
+def apply_clearance(cart)
+  cart.each do |item|
+    if cart[item[0]][:clearance] == true 
+      cart[item[0]][:price] = (cart[item[0]][:price] * 0.8).round(2)
     end
   end
 end
 
-def apply_coupons(cart, coupons)
-  # code here
-end
-
-def apply_clearance(cart)
-  # code here
-end
-
 def checkout(cart, coupons)
-  # code here
+  sCart = {}
+  sCart = consolidate_cart(cart)
+  sCartCoupons = apply_coupons(sCart, coupons)
+  sCartDiscounts = apply_clearance(sCartCoupons)
+  
+  total = 0
+  sCartDiscounts.keys.each do |item|
+    total += sCartDiscounts[item][:price]*sCartDiscounts[item][:count]
+  end
+
+  if total > 100 
+    (total * 0.90).round(2)
+  else
+    total
+  end
 end
